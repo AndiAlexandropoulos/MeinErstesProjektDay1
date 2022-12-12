b27e3ad64507fff9af9d25cc72f5679b34c9b2fa
AndiAlexandropoulos <andiamarbeiten@gmail.com>
2022-12-12 11:37:06 +0100

    Initial Commit

---
+++ MeinErstesProjektDay1/MeinErstesProjektDay1App.swift
@@ -0,0 +1,17 @@
+//
+//  MeinErstesProjektDay1App.swift
+//  MeinErstesProjektDay1
+//
+//  Created by andi a on 12.12.22.
+//
+
+import SwiftUI
+
+@main
+struct MeinErstesProjektDay1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
