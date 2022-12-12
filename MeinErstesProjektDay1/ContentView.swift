0a6eaa851c744499b7402e56e97dc513669b7c1f
AndiAlexandropoulos <andiamarbeiten@gmail.com>
2022-12-12 11:51:40 +0100

    first change

--- MeinErstesProjektDay1/ContentView.swift
+++ MeinErstesProjektDay1/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
         .padding()
     }
 }
-
+// Vorschau?
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
